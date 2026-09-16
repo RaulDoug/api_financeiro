3fa046c4c55e8849396623f9a1a4c40479cc4fc2
Raul <rauldougbr@gmail.com>
2026-09-16 18:52:55 -0300

    Configurações para deploy render

--- src/config/db.js
+++ src/config/db.js
@@ -4,12 +4,21 @@ const { Pool } = pg;
 
 pg.types.setTypeParser(1700, (val) => parseFloat(val));
 
+const isProduction = process.env.NODE_ENV === 'production';
+
+const dbConfig = process.env.DATABASE_URL
+  ? { connectionString: process.env.DATABASE_URL }
+  : {
+    user: process.env.DB_USER,
+    password: process.env.DB_PASSWORD,
+    host: process.env.DB_HOST,
+    port: process.env.DB_PORT,
+    database: process.env.DB_DATABASE,
+  };
+
 const pool = new Pool({
-  user: process.env.DB_USER,
-  password: process.env.DB_PASSWORD,
-  host: process.env.DB_HOST,
-  port: process.env.DB_PORT,
-  database: process.env.DB_DATABASE,
+  ...dbConfig,
+  ssl: isProduction ? { rejectUnauthorized: false } : false,
 });
 
 export default pool;
